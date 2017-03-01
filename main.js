35cf2dd69d39b0d29f05d1160f850ea6521faa97
Zak Clough <cameron.clough@mtchs.org>
2017-02-28 20:04:14 -0700

    Updated JavaScript
    Fixed error with repeated answers in Multiple Choice.

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-var op = ['+', '-', '*', '/', '%'];
+// Zak Clough
+// Math Quiz
+
+// Arrays and Modulus and Variables and Constant
+const op = ['+', '-', '*', '/', '%'];
 var questions = [];
 var answers = [];
 var multAns1 = [];
@@ -10,19 +14,39 @@ var wrong = 0;
 var j = 0;
 var i = 1;
 
+/* ---Get Current Date--- */
+function date() {
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //January is 0!
+    var yyyy = today.getFullYear();
+    if(dd < 10) {
+        dd = '0' + dd
+    } 
+    if(mm<10) {
+        mm = '0' + mm
+    }
+    today = mm + '/' + dd + '/' + yyyy;
+    document.getElementById("date").innerHTML = today;
+}
+
+/* ---Start The Quiz--- */
 function startQuiz(a, b) {
     var type = document.getElementById("ToQ").value;
     document.getElementById("userInput").value = '';
     var y = op[a];
     if (b < 1 || b > 10) {
+        // Error Checking
         window.alert("Plase input a valid number of questions between 1 and 10")
     } else {
     if (type === "md") {
         while (i <= b) {
+            // Math Functions and Random Function
             var x = Math.floor(Math.random() * 10) + 1;
             var z = Math.floor(Math.random() * 10) + 1;
+            // Decimal Format if Needed
             var ans = Math.round(eval(x + y + z) * 100) / 100;
-            questions.push(i + '. ' + x + ' ' + y + ' ' + z + ' = ');
+            questions.push(i + ') ' + x + ' ' + y + ' ' + z + ' = ');
             answers.push(ans);
             i++;
         }
@@ -36,20 +60,47 @@ function startQuiz(a, b) {
     }
     }
 }
+/* ---Check For Duplicate Answers--- */
+// Returning Values
+function checkAns(m, n) {
+    if (m == n) {
+        return true;
+    } else {
+        return false;
+    }
+}
+/* ---Generate Multiple Choice Questions--- */
 function choice(y, q) {
-    while(i <= q) {
+    // For Loop
+    for(i = 0; i < q; i++) {
         var pos = Math.floor(Math.random() * (3-0 + 1) + 0);
         var x = Math.floor(Math.random() * 10) + 1;
         var z = Math.floor(Math.random() * 10) + 1;
         var ans = Math.round(eval(x + y + z) * 100) / 100;
+        var k = i + 1;
         ansPos.push(pos);
-        questions.push(i + '. ' + x + ' ' + y + ' ' + z + ' = ');
+        questions.push(k + ') ' + x + ' ' + y + ' ' + z + ' = ');
         answers.push(ans);
         multAns1.push(ans + (Math.floor(Math.random() * 10) + 4));
         multAns2.push(ans - (Math.floor(Math.random() * 10) + 3));
         multAns3.push(ans + (Math.floor(Math.random() * 10) + 2));
         multAns4.push(ans - (Math.floor(Math.random() * 10) + 1));
-        i++;
+        while(checkAns(multAns1[i], ans[i]) == true) {
+            multAns1.pop();
+            multAns1.push(ans + (Math.floor(Math.random() * 10) + 4));
+        }
+        while(checkAns(multAns2[i], ans[i]) == true) {
+            multAns2.pop();
+            multAns2.push(ans - (Math.floor(Math.random() * 10) + 3));
+        }
+        while(checkAns(multAns3[i], ans[i]) == true) {
+            multAns3.pop();
+            multAns3.push(ans + (Math.floor(Math.random() * 10) + 2));
+        }
+        while(checkAns(multAns4[i], ans[i]) == true) {
+            multAns4.pop();
+            multAns4.push(ans - (Math.floor(Math.random() * 10) + 1));
+        }
     }
     document.getElementById("test2").innerHTML = questions[0];
     document.getElementById("one").innerHTML = multAns1[0];
@@ -62,6 +113,7 @@ function choice(y, q) {
     choice.style.display = "block";
     placeAns();
 }
+/* ---Place Random Answer For Multiple Choice--- */
 function placeAns() {
     if (ansPos[j] === 0) {
         document.getElementById("one").innerHTML = answers[j];
@@ -73,9 +125,10 @@ function placeAns() {
         document.getElementById("four").innerHTML = answers[j];
     }
 }
+/* ---Check Answers To See If Users Are Correct--- */
 function checkAnswer() {
     var type = document.getElementById("ToQ").value;
-    if (type === "md") {
+    if (type === "md" || type != "mc") {
         var answer = answers[j];
         var input = document.getElementById("userInput").value;
     } else {
@@ -94,6 +147,10 @@ function checkAnswer() {
                 document.getElementById("userInput").value = ''; 
             } else {
                 document.getElementById("test2").innerHTML = questions[j];
+                document.getElementById("one").innerHTML = multAns1[j];
+                document.getElementById("two").innerHTML = multAns2[j];
+                document.getElementById("three").innerHTML = multAns3[j];
+                document.getElementById("four").innerHTML = multAns4[j];
                 placeAns();
             }
         }
@@ -102,6 +159,7 @@ function checkAnswer() {
         window.alert("Please Try Again");
     }
 }
+/* ---Show Final Score--- */
 function score() {
     var result = document.getElementById("end");
     var drill = document.getElementById("drill");
@@ -120,6 +178,7 @@ function score() {
     document.getElementById("questions").innerHTML = questions.length;
     
 }
+/* ---Reset Program--- */
 function restart() {
     var result = document.getElementById("end");
     var sp = document.getElementById("startPrompt");
@@ -137,6 +196,7 @@ function restart() {
     i = 1;
     document.getElementById("quest").value = '';
 }
+/* ---JQuery Submit On Enter Press--- */
 $(document).ready(function(){
 $('#userInput').keypress(function(e){
   if(e.keyCode==13)
